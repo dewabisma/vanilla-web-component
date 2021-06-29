82d60baaacc79e76faa450aa458e2fbdd710f6c5
Dewa Bisma <idewagedebisma@gmail.com>
2021-06-29 17:58:30 +0800

    finish dropdown component

--- dropdown/dropdown.js
+++ dropdown/dropdown.js
@@ -2,4 +2,18 @@ const dropdownBtnList = document.querySelectorAll('[data-toggle="dropdown"]');
 
 const toggleDropdown = (dropdownBtn) => {
   const dropdownList = dropdownBtn.nextElementSibling;
+
+  if (dropdownBtn.ariaExpanded === "false") {
+    dropdownList.style.display = "block";
+    dropdownBtn.ariaExpanded = "true";
+  } else {
+    dropdownList.style.display = "none";
+    dropdownBtn.ariaExpanded = "false";
+  }
 };
+
+dropdownBtnList.forEach((btn) =>
+  btn.addEventListener("click", (e) => {
+    toggleDropdown(btn);
+  })
+);
