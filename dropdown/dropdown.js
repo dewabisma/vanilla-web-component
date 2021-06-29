48ea95865c4ac34e47d1760c6acd8ac5d0a5e289
Dewa Bisma <idewagedebisma@gmail.com>
2021-06-29 15:34:59 +0800

    base file for dropdown component

---
+++ dropdown/dropdown.js
@@ -0,0 +1,5 @@
+const dropdownBtnList = document.querySelectorAll('[data-toggle="dropdown"]');
+
+const toggleDropdown = (dropdownBtn) => {
+  const dropdownList = dropdownBtn.nextElementSibling;
+};
